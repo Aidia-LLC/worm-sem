d91efb53d31494912614cf726e4645fe4f4b612a
Kollin Murphy <kollin.murphy@gmail.com>
2023-05-05 13:34:26 -0600

    csharp

--- csharp/commands/GetParamCommand.cs
+++ csharp/commands/GetParamCommand.cs
@@ -18,6 +18,16 @@ namespace wormsem.commands
             try
             {
                 object response = api.GetParam(param);
+                if (response is String)
+                {
+                    if (((string)response).Equals("Live"))
+                    {
+                        return new SuccessResponse(id, "Successfully got param " + param, SuccessCode.GET_PARAM_SUCCESS, param + "=0");
+                    } else if (((string)response).Equals("Frozen"))
+                    {
+                        return new SuccessResponse(id, "Successfully got param " + param, SuccessCode.GET_PARAM_SUCCESS, param + "=1");
+                    }
+                }
                 return new SuccessResponse(id, "Successfully got param " + param, SuccessCode.GET_PARAM_SUCCESS, param + "=" + response);
             }
             catch (Exception err)
