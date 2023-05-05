c2b9f0ecc6ae34da56d07cb4effe2f1dccbff03a
Kollin Murphy <kollin.murphy@gmail.com>
2023-05-05 13:59:02 -0600

    csharp

--- app/src/data/createOptionsStore.ts
+++ app/src/data/createOptionsStore.ts
@@ -2,19 +2,19 @@ import { ProcessingOptions } from "@dto/ProcessingOptions";
 import { createStore, SetStoreFunction } from "solid-js/store";
 
 const defaultOptions: ProcessingOptions = {
-  squareSize: 420,
+  squareSize: 250,
   gaussianKernel: [0.06242931069425457, 0.1247976249479739, 0.2524974040020353],
-  hysteresisHigh: 0.075,
-  hysteresisLow: 0.012,
+  hysteresisHigh: 0.045,
+  hysteresisLow: 0.005,
   minNeighborsForNoiseReduction: 6,
   houghVoteThreshold: 0.65,
   mergeThetaThreshold: 10,
   pixelThreshold: 0.35,
   maxLines: 6,
   noiseReductionIterations: 15,
-  densityThreshold: 0.3080404237765713,
-  densityStep: 2,
-  densitySize: 16,
+  densityThreshold: 0.6,
+  densityStep: 6,
+  densitySize: 35,
   minimumFit: 0.2,
   firstFit: 25,
 };
