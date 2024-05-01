60e69fccb8b86d387f5cc2f12c54d20608370450
Asher Gunsay <ashergunsay@Ashers-MacBook-Pro.local>
2024-05-01 12:07:14 -0600

    python setup

---
+++ setup.sh
@@ -0,0 +1,5 @@
+# Set up python
+
+pip install flask flask-cors opencv-python pycocotools matplotlib onnxruntime onnx torch torchvision
+pip install git+https://github.com/facebookresearch/segment-anything.git
+curl -o ./mask-serveram_vit_h_4b8939.pth https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
