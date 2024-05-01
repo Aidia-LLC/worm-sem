d41115c25771c0ae0277a8558a62c3ac4ddeabd0
Asher Gunsay <ashergunsay@Ashers-MacBook-Pro.local>
2024-05-01 12:20:06 -0600

    esbuild dep

--- setup.sh
+++ setup.sh
@@ -1,5 +1,50 @@
 # Set up python
 
+echo "0 / X: INSTALLATION CONFIRMATION"
+echo "•"
+echo "•"
+echo "•"
+echo "•"
+echo "•"
+
+while true; do
+    read -p "Have you installed python version 3.9?" yn
+    case $yn in
+        [Yy]* ) make install; break;;
+        [Nn]* ) echo "Please install python v3.9." exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+while true; do
+    read -p "Have you installed dotnet already?" yn
+    case $yn in
+        [Yy]* ) make install; break;;
+        [Nn]* ) echo "Please install dotnet before proceeding." exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+while true; do
+    read -p "Is the Zeiss Remote API software available (usb is plugged in)?" yn
+    case $yn in
+        [Yy]* ) make install; break;;
+        [Nn]* ) echo "Please install dotnet before proceeding." exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# Set up python
+
+echo "1 / X: PYTHON SETUP"
+echo "•"
+echo "•"
+echo "•"
+echo "•"
+echo "•"
+
 pip install flask flask-cors opencv-python pycocotools matplotlib onnxruntime onnx torch torchvision
 pip install git+https://github.com/facebookresearch/segment-anything.git
-curl -o ./mask-serveram_vit_h_4b8939.pth https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+curl -o ./mask-server/sam_vit_h_4b8939.pth https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+
+echo "2 / X: "
